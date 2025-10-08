240fa86b67c706c72d34b4339e69d3bbf04d0cf3
Oleg <olegmaslakov47@gmail.com>
2025-10-08 23:02:12 +0300

    Add item context

--- contacts/src/components/ContactItem/ContactItem.jsx
+++ contacts/src/components/ContactItem/ContactItem.jsx
@@ -1,1 +1,16 @@
 
+import { Component } from 'react'
+
+export class ContactItem extends Component {
+  render() {
+    const {fName,lName} = this.props.contact;
+    return (
+      <div>
+        <p>{fName} {lName}</p>
+        <span>X</span>
+      </div>
+    )
+  }
+}
+
+export default ContactItem
