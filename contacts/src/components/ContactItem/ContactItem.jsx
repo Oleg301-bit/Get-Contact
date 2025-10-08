c312340a946868e1d4d3e46ec2e7602c94dc0abc
Oleg <olegmaslakov47@gmail.com>
2025-10-08 23:26:45 +0300

    Add item css

--- contacts/src/components/ContactItem/ContactItem.jsx
+++ contacts/src/components/ContactItem/ContactItem.jsx
@@ -1,13 +1,14 @@
-
 import { Component } from 'react'
+import './ContactItem.css';
+
 
 export class ContactItem extends Component {
   render() {
     const {fName,lName} = this.props.contact;
     return (
-      <div>
-        <p>{fName} {lName}</p>
-        <span>X</span>
+      <div className='watch-item'>
+        <p className='content'>{fName} {lName}</p>
+        <span className='delete-btn'>X</span>
       </div>
     )
   }
--- contacts/src/components/ContactList/ContactList.jsx
+++ contacts/src/components/ContactList/ContactList.jsx
@@ -1,15 +1,17 @@
 import { Component } from 'react';
 import ContactItem from '../ContactItem/ContactItem';
 
+
+
 export class ContactList extends Component {
   render() {
     return (
-      <>
+      <div>
         {this.props.contacts.map((contact) => {
           return (<ContactItem key={contact.id} contact={contact} />);
         })}
         <button>New</button>
-      </>
+      </div>
     );
   }
 }
